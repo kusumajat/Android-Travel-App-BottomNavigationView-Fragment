1ee422ee5d9ba4badedc6bd983cefbf530789a62
Kusumajati <rkusumajati14@gmail.com>
2024-10-22 23:07:45 +0700

    travel-app

---
+++ app/src/main/java/com/example/navigation_pgpb_vii/CartFragment.java
@@ -0,0 +1,23 @@
+package com.example.navigation_pgpb_vii;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CartFragment extends Fragment {
+
+    public CartFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate layout for CartFragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
